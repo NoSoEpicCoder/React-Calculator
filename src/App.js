6f5f373701a97695d28f1356bb6e3934a1ca8e82
Kevin <kevingilbert@ghost.local>
2019-04-15 20:11:00 -0400

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import KeyPadComponent from './components/KeyPadComponent';
+import ResultComponent from './components/ResultComponent';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      result: ""
+    }
+  }
+
+  onClick = button => {
+    if(button === "="){
+      this.calculate()
+    } else if(button === "C"){
+      this.reset()
+    } else if(button === "CE"){
+      this.backspace()
+    } else {
+      this.setState({
+        result: this.state.result + button
+      })
+    }
+  };
+
+  calculate = () => {
+    try{
+      this.setState({
+        result: (eval(this.state.result) || "") + ""
+      })
+    } catch(e){
+      this.setState({
+        result: "error"
+      })
+    }
+  };
+
+  reset = () => {
+    this.setState({
+      result: ""
+    })
+  };
+
+  backspace = () => {
+    this.setState({
+      result: this.state.result.slice(0, -1)
+    })
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h2>Superb Calculator</h2>
+        <div id="calculator-body">
+          <ResultComponent result={this.state.result}/>
+          <KeyPadComponent onClick={this.onClick}/>
+        </div>
       </div>
     );
   }
